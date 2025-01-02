81404ed45e0cb09b4edb48309e7460f9dfa30979
pranavkhaspa <khaspa.pranav1@gmail.com>
2025-01-02 18:23:36 +0530

    leaderboard

--- guess_number.py
+++ guess_number.py
@@ -1,4 +1,5 @@
 import random
+import os
 def guess(a,b,name="player"):
     guess=0
     number_of_tries=0
@@ -13,14 +14,39 @@ def guess(a,b,name="player"):
         elif(guess==random_number):
             print(f"{name} you guessed it right in {number_of_tries} tries!!")
             break
-        
-            
-            
-         
-        
-        
-if __name__=="__main__":
+    score(name,number_of_tries)
+def score(name, n):
+    try:
+        f = open("scores.csv", "r")
+        lines = f.readlines()
+        f.close()
 
+        highScore = None
+
+        if lines:
+            firstLine = lines[0].strip().split(",")
+            highScore =  int(firstLine[1])
+
+        if highScore is None or n < highScore:
+            f= open("scores.csv", "w") 
+            f.write(f"{name},{n}\n")
+            f.close()
+
+            if highScore is not None:
+                    f.writelines(lines)  
+        else:
+            f= open("scores.csv", "a")
+            f.write(f"{name},{n}\n")
+            f.close()
+
+    except FileNotFoundError:
+        f= open("scores.csv", "x")
+        f.write(f"name,Leaderboard position\n{name},{n}\n")
+        f.close()
+
+            
+if __name__=="__main__":
+    n=input("Enter your name >")
     a=int(input("Enter the starting range >"))
     b=int(input("Enter the end range >"))
-    guess(a,b)
+    guess(a,b,n)
